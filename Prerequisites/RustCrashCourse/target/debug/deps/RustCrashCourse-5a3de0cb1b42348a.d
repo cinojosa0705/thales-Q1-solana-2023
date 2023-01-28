e298abc1efbf0bfe15bd72ba259542ef6b80fc40
Thales <81639334+cinojosa0705@users.noreply.github.com>
2023-01-27 23:43:35 -0500

    struct.rs

--- Prerequisites/RustCrashCourse/target/debug/deps/RustCrashCourse-5a3de0cb1b42348a.d
+++ Prerequisites/RustCrashCourse/target/debug/deps/RustCrashCourse-5a3de0cb1b42348a.d
@@ -1,6 +1,6 @@
-/Users/carlito.inojosa/W3A/thales-Q1-solana-2023/Prerequisites/RustCrashCourse/target/debug/deps/RustCrashCourse-5a3de0cb1b42348a: src/main.rs src/print.rs src/vars.rs src/types.rs src/strings.rs src/tuples.rs src/arrays.rs src/vectors.rs src/conditionals.rs src/loops.rs src/functions.rs src/pointer_ref.rs
+/Users/carlito.inojosa/W3A/thales-Q1-solana-2023/Prerequisites/RustCrashCourse/target/debug/deps/RustCrashCourse-5a3de0cb1b42348a: src/main.rs src/print.rs src/vars.rs src/types.rs src/strings.rs src/tuples.rs src/arrays.rs src/vectors.rs src/conditionals.rs src/loops.rs src/functions.rs src/pointer_ref.rs src/structs.rs
 
-/Users/carlito.inojosa/W3A/thales-Q1-solana-2023/Prerequisites/RustCrashCourse/target/debug/deps/RustCrashCourse-5a3de0cb1b42348a.d: src/main.rs src/print.rs src/vars.rs src/types.rs src/strings.rs src/tuples.rs src/arrays.rs src/vectors.rs src/conditionals.rs src/loops.rs src/functions.rs src/pointer_ref.rs
+/Users/carlito.inojosa/W3A/thales-Q1-solana-2023/Prerequisites/RustCrashCourse/target/debug/deps/RustCrashCourse-5a3de0cb1b42348a.d: src/main.rs src/print.rs src/vars.rs src/types.rs src/strings.rs src/tuples.rs src/arrays.rs src/vectors.rs src/conditionals.rs src/loops.rs src/functions.rs src/pointer_ref.rs src/structs.rs
 
 src/main.rs:
 src/print.rs:
@@ -14,3 +14,4 @@ src/conditionals.rs:
 src/loops.rs:
 src/functions.rs:
 src/pointer_ref.rs:
+src/structs.rs:
